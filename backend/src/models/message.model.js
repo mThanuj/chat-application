ce0b39054f5eee6f509f42800dc936f6ce993a6b
mThanuj <mullagurithanuj0@gmail.com>
2024-12-26 12:51:31 +0530

    add socket.io

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
