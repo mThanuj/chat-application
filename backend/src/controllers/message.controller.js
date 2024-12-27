3f98917e0b705d16da7cc9cb7e20d0c6e86b3d6c
mThanuj <mullagurithanuj0@gmail.com>
2024-12-27 07:37:21 +0530

    add cloudinary and message routes

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,5 @@
+import asyncHandler from "../utils/asyncHandler.js";
+
+export const getMessages = asyncHandler(async (req, res) => {});
+
+export const sendMessage = asyncHandler(async (req, res) => {});
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { verify } from "../middleware/auth.middleware.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/:id", verify, getMessages);
+router.post("/send/:id", verify, sendMessage);
+
+export default router;
