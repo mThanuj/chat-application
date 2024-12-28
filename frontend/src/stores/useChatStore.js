a60bf35ff1ca0fc39f922fc331a4fc37bf4654d4
mThanuj <mullagurithanuj0@gmail.com>
2024-12-28 13:45:09 +0530

    nothing

--- frontend/src/stores/useChatStore.js
+++ frontend/src/stores/useChatStore.js
@@ -3,6 +3,10 @@ import { create } from "zustand";
 const useChatStore = create((set, get) => ({
   messages: [],
   receiver: null,
+
+  getCurrentReceiver: () => {
+    return get().receiver;
+  },
 }));
 
 export default useChatStore;
